c1f707a60114d3a0ee319da6087206c8c639f678
Lucas Ximenes <lucasximenes177@gmail.com>
2025-07-09 18:40:32 -0300

    feat: adiciona tela principal com botões de jogo e lógica conectada
    - Tela `MainScreen` com botões de reset, status e finalização
    - Integra `BoardService` e exibe mensagens com JOptionPane
    - Ajuste na `UiMain` pra iniciar a tela com args

--- src/main/java/ui/custom/screen/MainScreen.java
+++ src/main/java/ui/custom/screen/MainScreen.java
@@ -1,8 +1,83 @@
 package ui.custom.screen;
 
+import model.GameStatusEnum;
+import service.BoardService;
+import ui.custom.button.FinishGameButton;
+import ui.custom.button.ResetButton;
+import ui.custom.frame.MainFrame;
+import ui.custom.panel.MainPanel;
+
+import javax.swing.*;
 import java.awt.*;
+import java.util.Map;
 
 public class MainScreen {
 
     private final static Dimension dimension = new Dimension(600,600);
+
+    private final BoardService boardService;
+
+    private JButton finishGameButton;
+    private JButton checkGameStatusButton;
+    private JButton resetButton;
+
+    public MainScreen(final Map<String, String> gameConfig) {
+        this.boardService = new BoardService(gameConfig);
+    }
+
+    public void buildMainScreen() {
+        JPanel mainPanel = new MainPanel(dimension);
+        JFrame mainFrame = new MainFrame(dimension, mainPanel);
+        addResetButton(mainPanel);
+        addCheckGameStatusButton(mainPanel);
+        addFinishGameButton(mainPanel);
+        mainFrame.revalidate();
+        mainFrame.repaint();
+    }
+
+    private void addFinishGameButton(JPanel mainPanel) {
+       finishGameButton = new FinishGameButton(e ->{
+            if(boardService.gameIsFinished()){
+                JOptionPane.showMessageDialog(null,"Parabéns, você concluiu o jogo!");
+                resetButton.setEnabled(false);
+                checkGameStatusButton.setEnabled(false);
+                finishGameButton.setEnabled(false);
+            }else{
+                JOptionPane.showMessageDialog(null, "Seu jogo tem alguma inconsistência, ajuste e tente novamente.");
+            }
+       });
+        mainPanel.add(finishGameButton);
+    }
+
+    private void addCheckGameStatusButton(JPanel mainPanel) {
+        checkGameStatusButton = new FinishGameButton(e -> {
+            boolean hasErrors = boardService.hasErrors();
+            GameStatusEnum gameStatus = boardService.getStatus();
+            var message = switch(gameStatus){
+                case NON_STARTED -> "O jogo não foi iniciado.";
+                case INCOMPLETE -> "O jogo está incompleto.";
+                case COMPLETE -> "O jogo está completo.";
+            };
+            message += hasErrors ? " e contém erros: " : "e não contém erros.";
+            JOptionPane.showMessageDialog(null, message);
+        });
+        mainPanel.add(MainScreen.this.checkGameStatusButton);
+    }
+
+    private void addResetButton(JPanel mainPanel) {
+        resetButton = new ResetButton(e -> {
+            int dialogResult = JOptionPane.showConfirmDialog(
+                    null,
+                    "Deseja realmente reiniciar o jogo?",
+                    "Limpar o jogo",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE
+            );
+            if(dialogResult == 0){
+                boardService.reset();
+
+            }
+        });
+        mainPanel.add(resetButton);
+    }
 }
